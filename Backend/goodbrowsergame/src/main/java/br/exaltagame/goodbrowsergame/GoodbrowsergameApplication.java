05fa645c9ee036115446f4ef27b41f2d3b8e602f
Luca Hummel <luca.hummel12@gmail.com>
2022-03-17 22:07:52 -0300

    Projeto criado

---
+++ Backend/goodbrowsergame/src/main/java/br/exaltagame/goodbrowsergame/GoodbrowsergameApplication.java
@@ -0,0 +1,13 @@
+package br.exaltagame.goodbrowsergame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoodbrowsergameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoodbrowsergameApplication.class, args);
+	}
+
+}
